2868f5195101b8950a4bd8d3d86599febd78040a
Henrique Luiz <heenluy@gmail.com>
2022-01-26 15:38:39 -0300

    Servidor de Configuração 'Spring Config Server' adicionado

---
+++ config-server/src/test/java/me/henriqueluiz/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.henriqueluiz.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
